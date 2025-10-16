0ebcdc6e26ecd75f25f9c2b34008be749a3913ee
Lorenzo Duranti <106395704+lorenzoduranti05@users.noreply.github.com>
2025-10-16 12:41:10 +0200

    struttura
    adada

---
+++ backend/itsProjectWork/src/test/java/dev2426/itsProjectWork/ItsProjectWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev2426.itsProjectWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsProjectWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

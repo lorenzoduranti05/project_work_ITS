a76ff0f256eb3379d70cb5c6d53027a4c7963a3f
Nicesp05-digital <nesposito@studenti.laziodigital.it>
2025-10-16 14:35:39 +0200

    Update HomeController.java

--- backend/itsProjectWork/src/main/java/dev2426/itsProjectWorkController/HomeController.java
+++ backend/itsProjectWork/src/main/java/dev2426/itsProjectWorkController/HomeController.java
@@ -4,13 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-	public class HomeController {
+public class HomeController {
 
-		@GetMapping("/home")
-		public String mostraPagina() {
-			return "homepage.html";
-		}
-
-	
+	@GetMapping("/home")
+	public String mostraPagina() {
+		return "homepage.html";
+	}
 
 }
